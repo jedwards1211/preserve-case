db0c1379fd0c80f04265e8fdda296c3020f6b9fe
Andy Edwards <jedwards@fastmail.com>
2017-06-15 19:41:41 -0500

    feat(lib/index.js): initial commit

--- test/index.js
+++ test/index.js
@@ -1,8 +1,23 @@
-require('../lib')
+var replace = require('..')
+var expect = require('chai').expect
 
-describe('test setup', function () {
+describe('replace', function () {
   it('works', function () {
+    expect(
+      replace('  foo bar   FOO_BAR  foo_bar FooBar ', 'foo bar', 'hello world')
+    ).to.equal(
+      '  hello world   HELLO_WORLD  hello_world HelloWorld '
+    )
 
+    expect(
+      replace(
+        '  foo bar   FOO_BAR  baz_bar  fooBaz ',
+        /(foo|bar|baz)[-_ ]?(foo|bar|baz)/ig,
+        function (_, a, b) { return b + ' ' + a }
+      )
+    ).to.equal(
+      '  bar foo   BAR_FOO  bar_baz  bazFoo '
+    )
   })
 })
 
